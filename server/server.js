2d5a6ba0ca3dbf111e8ddb397abd48966884a296
YUVRAJSINH JADEJA <mr.yuvrajsinhjadeja007@gmail.com>
2025-12-26 16:59:15 +0530

    Initial

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use('/api/user', userRouter);
 app.use('/api/owner', ownerRouter);
 app.use('/api/bookings', bookingRouter); 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Start the server
 app.listen(PORT, () => {
